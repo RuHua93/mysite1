ea51c51ef1e499b48b5598c86a6a9471e3b3fef6
twt <1377849411@qq.com>
2016-04-25 10:08:04 +0800

    画了一些素材: 下一步:功能\数据库\UI\详细设计

---
+++ mysite/views.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+
+import sys
+
+# 默认编码改成utf8,不然会报错
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def welcome(req):
+    return render_to_response('welcome.html')
