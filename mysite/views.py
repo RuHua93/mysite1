12fce90744993ddd76dca3adfea784fb45531be0
twt <1377849411@qq.com>
2016-04-28 10:10:58 +0800

    阶段性保存.下一步:资源库(分页)\hot top\个人中心

--- mysite/views.py
+++ mysite/views.py
@@ -22,16 +22,40 @@ def welcome(req):
 
 def login(req):
     # return render_to_response('login.html',{'csrf_token':'T83ow7BDm8RlRP8Hxj622rKExUqyHAm1'})
+
     return render_to_response('login.html',{},context_instance=RequestContext(req))
 
 def register(req):
+
     return render_to_response('register.html',{},context_instance=RequestContext(req))
 
 def show(req):
-    return render_to_response('show.html')
+    username=req.COOKIES.get("username")
+    conn = sqlite3.connect("/tmpdb/test.db")
+    csr = conn.cursor()
+    csr.execute("select count(*) from sqlDemo")
+    cnt = csr.fetchall()[0][0]
+    pcnt = (cnt + 10) / 10
+    return render_to_response('show.html',{"username":username, "pcnt":pcnt},context_instance=RequestContext(req))
 
 def doreg(req):
     # do reg
+    username = req.POST.get('uname')
+    email = req.POST.get('uemail')
+    password = req.POST.get('upwd')
+    conn = sqlite3.connect("/tmpdb/tb.db")
+    csr = conn.cursor()
+    csr.execute("select * from user where username=?", (username,))
+    recs = csr.fetchall()
+    if recs:
+        return render_to_response('register.html', {"regstat":"fail", "str":"用户名已存在"},context_instance=RequestContext(req))
+    csr.execute("select * from user where email=?", (email,))
+    recs = csr.fetchall()
+    if recs:
+        return render_to_response('register.html', {"regstat":"fail", "str":"该邮箱已注册"},context_instance=RequestContext(req))
+    conn.execute("insert into user(username, password, email) values(?, ?, ?);", (username, password, email,))
+    print "done"
+    conn.commit()
     return render_to_response('login.html',{"regstat":"ok"},context_instance=RequestContext(req))
     # str =  "<div class=\"alert alert-error alert-dismissable\">" \
     #        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" onclick" \
@@ -43,9 +67,23 @@ def regok(req):
 
 def dolog(req):
     if req.method == 'POST':
-        print 1
-        return HttpResponseRedirect('show')
-
-    print 2
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+        conn = sqlite3.connect("/tmpdb/tb.db")
+        conn.row_factory = sqlite3.Row
+        csr = conn.cursor()
+        print username
+        csr.execute("select id, password, username from user where username=?", (username,))
+        recs = csr.fetchall()
+        if recs:
+            print 1
+            for rec in recs:
+                if rec["password"] == password:
+                    print rec["id"]
+                    response = render_to_response('show.html', {"logstat":"ok", "uid":rec["id"], "username":rec["username"]})
+                    response.set_cookie('uid', rec["id"], 3600)
+                    response.set_cookie('username', rec["username"], 3600)
+                    return response
+        return render_to_response("login.html", {"logstat":"fail", "str":"用户名或密码不正确"},context_instance=RequestContext(req))
     # render_to_response("test.html",{},context_instance=RequestContext(req))
     return render_to_response('show.html',{},context_instance=RequestContext(req))
